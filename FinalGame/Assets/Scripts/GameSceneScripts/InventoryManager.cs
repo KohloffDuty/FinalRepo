e73f2da859a5c3a6cafd331629f1d75325db49d3
PalesaDM <palesamasimene@gmail.com>
2024-10-11 18:14:55 +0200

    im not sure
    Co-Authored-By: KohloffDuty <162119819+KohloffDuty@users.noreply.github.com>
    Co-Authored-By: Bbygirldunk <167876398+Bbygirldunk@users.noreply.github.com>

--- FinalGame/Assets/Scripts/GameSceneScripts/InventoryManager.cs
+++ FinalGame/Assets/Scripts/GameSceneScripts/InventoryManager.cs
@@ -1,49 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Generic;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class InventoryManager : MonoBehaviour
 {
-    public List<Item> inventory = new List<Item>();
-    public GameObject inventoryPanel;
-    public GameObject descriptionPanel;
-
-    private Item currentItem;
-
-    public void AddItem(Item item)
+    // Start is called before the first frame update
+    void Start()
     {
-        inventory.Add(item);
-        ShowItemDescription(item);
+        
     }
 
-    public void RemoveItem(Item item)
+    // Update is called once per frame
+    void Update()
     {
-        inventory.Remove(item);
-        // Logic to place item back in the world if needed
-    }
-
-    public void ShowItemDescription(Item item)
-    {
-        currentItem = item;
-        descriptionPanel.SetActive(true);
-        inventoryPanel.SetActive(false);
-        // Display item description in UI (You can add Text components in description panel)
-    }
-
-    public void ConfirmAddToInventory()
-    {
-        descriptionPanel.SetActive(false);
-        inventoryPanel.SetActive(true);
-        // Add to inventory UI
-    }
-
-    public void PutItemBack()
-    {
-        descriptionPanel.SetActive(false);
-        // Logic to place item back in the world
+        
     }
 }
-

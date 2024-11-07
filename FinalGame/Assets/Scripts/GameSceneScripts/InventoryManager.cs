a432e0e1ec4455e09b00ed2bf18780a94e37d003
Bbygirldunk <2708798@students.wits.ac.za>
2024-11-07 04:59:43 +0200

    Inventory system first attempt
    Co-Authored-By: KohloffDuty <162119819+KohloffDuty@users.noreply.github.com>
    Co-Authored-By: PalesaDM <167359449+PalesaDM@users.noreply.github.com>

--- FinalGame/Assets/Scripts/GameSceneScripts/InventoryManager.cs
+++ FinalGame/Assets/Scripts/GameSceneScripts/InventoryManager.cs
@@ -1,18 +1,58 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryManager : MonoBehaviour
+public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Item> items = new List<Item>();
+    public int maxGrenades = 5; // Limit for grenades
+    public int maxPowerUps = 3; // Limit for power-ups
+
+    // Adds an item to the inventory
+    public void AddItem(Item item)
     {
-        
+        Item existingItem = items.Find(i => i.itemName == item.itemName);
+
+        if (existingItem != null)
+        {
+            // Update quantity if the item already exists in the inventory
+            existingItem.quantity += item.quantity;
+        }
+        else
+        {
+            // Add new item if it doesn't exist in the inventory
+            items.Add(item);
+        }
+
+        // Enforce maximum limits
+        EnforceLimits(item);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EnforceLimits(Item item)
     {
-        
+        if (item.itemType == ItemType.Grenade && item.quantity > maxGrenades)
+        {
+            item.quantity = maxGrenades;
+        }
+        else if (item.itemType == ItemType.PowerUp && item.quantity > maxPowerUps)
+        {
+            item.quantity = maxPowerUps;
+        }
+    }
+
+    // Use an item, e.g., throw a grenade or activate a power-up
+    public void UseItem(string itemName)
+    {
+        Item item = items.Find(i => i.itemName == itemName);
+        if (item != null && item.quantity > 0)
+        {
+            // Logic to use the item in-game
+            item.quantity--;
+
+            // Remove item if quantity reaches zero
+            if (item.quantity <= 0)
+            {
+                items.Remove(item);
+            }
+        }
     }
 }
---
+++ FinalGame/Assets/Scripts/GameSceneScripts/Item.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public enum ItemType { Grenade, PowerUp, Weapon }
+
+[System.Serializable]
+public class Item
+{
+    public string itemName;
+    public ItemType itemType;
+    public int quantity;
+    public Sprite icon;
+
+    public Item(string name, ItemType type, int quantity, Sprite icon)
+    {
+        itemName = name;
+        itemType = type;
+        this.quantity = quantity;
+        this.icon = icon;
+    }
+}

6de58b1a25c8a5c8d5dfd23288b11c728c592b6b
KohloffDuty <162119819+KohloffDuty@users.noreply.github.com>
2024-11-11 10:26:01 +0200

    idek
    Co-Authored-By: Bbygirldunk <167876398+Bbygirldunk@users.noreply.github.com>
    Co-Authored-By: PalesaDM <167359449+PalesaDM@users.noreply.github.com>

--- FinalGame/Assets/Scripts/GameSceneScripts/FirstPersonControl.cs
+++ FinalGame/Assets/Scripts/GameSceneScripts/FirstPersonControl.cs
@@ -52,7 +52,7 @@ public class FirstPersonControl : MonoBehaviour
     public Material switchMaterial;
     public GameObject[] objectsToChangeColor;
 
-    public Inventory Inventory;
+   
 
 
     private void Awake()
